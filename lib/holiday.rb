fa6b0500bbd6e139425e3e1efc9f29b32bf299f6
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-19 21:53:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,9 +76,9 @@ def all_supplies_in_holidays(holiday_hash)
         holiday_array = holidays.split("_")
         holiday_array.each { |word| word.capitalize! }
         holiday_array.join(",")
-        puts "  #{holiday_array}: #{holiday.values.to_s}"
+        puts "  #{holiday_array}: #{holiday.values.flatten.to_s}"
       else
-        puts "  #{holidays.to_s.capitalize!}: #{holiday.values.to_s}"
+        puts "  #{holidays.to_s.capitalize!}: #{holiday.values.flatten.to_s}"
       end
     end
   end
