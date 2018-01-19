3f2d817efcae9820c6751f8cf2e915dc814d5bc6
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-19 21:12:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday.values.flatten
+      return holiday.values.flatten
     end
   end
 end
